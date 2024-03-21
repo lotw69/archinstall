e80e1f9326e69f8eaecd98d031064944c97f613b
lotw69 <53122963+lotw69@users.noreply.github.com>
2024-03-21 08:56:44 -0700

    testing default theme for Cinnamon

--- setup.sh
+++ setup.sh
@@ -381,6 +381,7 @@ function CINNANON_DE () {
     sudo pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter lightdm-webkit2-greeter lightdm-gtk-greeter-settings cinnamon gnome-disk-utility gnome-system-monitor gnome-calculator gpicview gedit variety onboard ark file-roller unrar p7zip nemo-fileroller nemo-share kitty kitty-terminfo kitty-shell-integration
     $ZB -S --noconfirm --needed gvfs-smb cinnamon-sounds mint-x-icon mint-themes mint-y-icons mint-artwork
     sudo systemctl enable lightdm
+    gsettings set org.cinnamon.theme name 'Mint-Y-Aqua'
 }
 
 ################################################################################

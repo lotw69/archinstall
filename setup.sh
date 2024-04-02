8c92b2ec182ddc7d23f57107abbf0954003f6985
lotw69 <53122963+lotw69@users.noreply.github.com>
2024-04-01 21:10:56 -0700

    Added some software for hyprland and fixes

--- setup.sh
+++ setup.sh
@@ -574,7 +574,7 @@ function HYPRLAND_DE(){
   sleep 2
   clear
   sudo pacman -S --noconfirm --needed hyprland hypridle xdg-desktop-portal-hyprland kitty dolphin gnome-disk-utility polkit sddm waybar breeze breeze-gtk breeze-icons kate nwg-look kservice5 kdbusaddons5 kfilemetadata5 kconfig5 kcoreaddons5 gwenview
-  $ZB -S --noconfirm --needed mugshot rofi-lbonn-wayland waypaper
+  $ZB -S --noconfirm --needed mugshot rofi-lbonn-wayland qt5ct-kde qt6ct-kde swww waypaper
   mkdir -p ~/.config/hypr
   cp hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
   mkdir -p ~/.config/kitty

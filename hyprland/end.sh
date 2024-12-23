b50ad47daab91bfe545f187c83e858834d8067c8
lotw69 <53122963+lotw69@users.noreply.github.com>
2024-12-22 23:28:25 -0800

    Working on fixing things thanks to updates

---
+++ hyprland/end.sh
@@ -0,0 +1,1 @@
+killall nwg-dock-hyprland
---
+++ hyprland/walp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+while true; do
+	wallpaper="$(find -L "/home/erik/Pictures/Wallpapers" -type f | shuf -n 1)"
+	echo "ipc = off
+
+	preload = $wallpaper
+
+	wallpaper = eDP-1,$wallpaper
+	wallpaper = HDMI-A-1,$wallpaper" > "/home/erik/.config/hypr/hyprpaper.conf"
+	hyprpaper &
+
+	sleep 900
+
+	killall -9 hyprpaper
+done

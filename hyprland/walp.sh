0b4b7d6fb8dafe4f2c694270631d63cb9944d9ac
lotw69 <53122963+lotw69@users.noreply.github.com>
2024-12-28 01:11:22 -0800

    Fixed for auto working
    All wallpapers should go into the users Pictures/Wallpaper directory or you can edit the location in the file, WPHOME

--- hyprland/walp.sh
+++ hyprland/walp.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+WPHOME=$HOME/Pictures/Wallpapers
+HLHOME=$HOME/.config/hypr/hyprpaper.conf
+
 while true; do
-	wallpaper="$(find -L "/home/erik/Pictures/Wallpapers" -type f | shuf -n 1)"
-	echo "ipc = off"
+
+	wallpaper="$(find -L $WPHOME -type f | shuf -n 1)"
+	echo "ipc = off
 
 	preload = $wallpaper
 
 	wallpaper = eDP-1,$wallpaper
-	wallpaper = HDMI-A-1,$wallpaper" > "/home/erik/.config/hypr/hyprpaper.conf"
+	wallpaper = HDMI-A-1,$wallpaper" > $HLHOME
 	hyprpaper &
 
 	sleep 900
